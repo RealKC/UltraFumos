b2cedc32d0a7e3b0e9ea21de56209d16df041798
Mițca Dumitru <dumitru0mitca@gmail.com>
2024-05-09 23:06:16 +0300

    Make it so the Unity project can actually build the AssetBundle needed

---
+++ FumoSkull/FumoSkull_Assets/Assets/Editor/AssetBundle.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class AssetBundle
+{
+    [MenuItem("Assets/Build Bundle")]
+    private static void BuildBundles()
+    {
+        var assetBundlePath = Application.dataPath + "/../AssetBundle";
+        Directory.CreateDirectory(assetBundlePath);
+        BuildPipeline.BuildAssetBundles(assetBundlePath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    }
+}
