1fb7e5c33c235276cf6336e7ad66006504d3c41c
Mițca Dumitru <dumitru0mitca@gmail.com>
2026-04-17 23:53:23 +0300

    WIP: Add a workflow to publish the mod
    Doesn't actually publish yet

--- Thunderstore/package.sh
+++
@@ -1,7 +0,0 @@
-#/usr/bin/env bash
-
-cd $(dirname $0)
-
-dotnet build --configuration Release ../FumoSkull/FumoSkull/FumoSkull.csproj
-
-zip package.zip CHANGELOG.md icon.png manifest.json README.md LICENSE.txt UltraFumos.dll
